0e9d02fd08a577289f5ac713a9c7f231528b8fdc
Hugo Kawamata <hugokawamata@gmail.com>
2017-07-14 11:27:18 +1000

    Still completing login boilerplate

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 __author__ = "Hugo Kawamata"
+import os
+import logging
+from flask import Flask, request, render_template
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 
-from flask import Flask
+from backend.models import *
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://DB_USER:PASSWORD@HOST/DATABASE'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+db.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id is None:
+        return None
+    try:
+        user = User.query.get(int(id))
+    except:
+        return None
+    return user
 
 
 ### Endpoints ###
@@ -12,8 +34,25 @@ app.config['DEBUG'] = True
 def main():
     return "Hello world!"
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']        
+        if User.query.filter(User.email == email).first().check_password(password):
+            user = User.query.filter(User.email == email).first()
+            login_user(user)
+            return redirect(request.args.get("next"))
+        else:
+            return abort(401)
+    else:
+        return render_template("login.html")
+
 
 ### Main Jeff ###
 
 if __name__ == "__main__":
     app.run()
+    muser = User("test@email.com", "password1")
+    db.session.add(muser)
+    db.session.commit()
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,26 @@
+import logging
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import flask_login
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    """
+    ORM Class: An object that represents a user (with all their game progess)
+    """
+    __tablename__ = "Users"
+    id              = db.Column('id',           db.Integer,             primary_key=True)
+    email           = db.Column('email',        db.String(128),         unique=True)
+    passwordhash    = db.Column('passwordhash', db.String(128))
+    gold            = db.Column('gold',         db.Integer)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.passwordhash = generate_password_hash(password)
+
+        logging.info("creating user with email and pw:" + email + " " + password)
+
+    def check_password(self, password_attempt):
+        return check_password_hash(self.passwordhash, password_attempt)
